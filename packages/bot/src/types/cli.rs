4cc3ee0a572c305aa4d96ab453316ec8efe91a87
av1ctor <av1ctortv@gmail.com>
2025-04-26 16:02:31 -0300

    fix: descriptions

--- packages/bot/src/types/cli.rs
+++ packages/bot/src/types/cli.rs
@@ -19,14 +19,14 @@ pub enum Commands {
     Search {
         #[arg(help = "Query used to search for meme templates")]
         query: String,
-        #[arg(default_value_t = 1, help = "Optional page number (default = 1)")]
+        #[arg(default_value_t = 1, help = "Optional page number (default: 1)")]
         page: usize,
     },
     #[command(about = "Generate a meme from a template")]
     Gen {
         #[arg(help = "Numeric id of the meme template as returned by '/meme search'")]
         id: u32,
-        #[arg(help = "Captions, separated by white-space, to be displayed on the image (use single-quotes, ie: 'some caption', to preserve white-spaces)")]
+        #[arg(help = "Captions, separated by white-space, to be displayed on the image (use single-quotes, e.g. 'a caption', to preserve white-spaces)")]
         captions: Vec<String>,
     },
     #[command(about = "Use AI to create a story and suggest captions for generating a meme from a template")]
@@ -45,7 +45,7 @@ pub enum Commands {
     },
     #[command(subcommand, about = "Sub-commands of the **Mementor Wallet**")]
     Wallet (Wallet),
-    #[command(subcommand, about = "Sub-commands of the **Mementor NFT collection**")]
+    #[command(subcommand, about = "Sub-commands of the **MEME NFT collection**")]
     Nft (Nft),
 }
 
@@ -57,49 +57,49 @@ pub enum Wallet {
     Address,
     #[command(about = "Withdraw ICP from your account in the Mementor Wallet")]
     Withdraw {
-        #[arg(help = "Amount to withdraw in decimal format (ie: 1.25)")]
+        #[arg(help = "Amount to withdraw in decimal format (eg: 1.25)")]
         amount: f32,
         #[arg(help = "Optional destination account address in hex format (default: your OC wallet)")]
         to: Option<String>,
     },
     #[command(about = "Display logs of ICP transactions")]
     Logs {
-        #[arg(default_value_t = 1, help = "Optional page number (default = 1)")]
+        #[arg(default_value_t = 1, help = "Optional page number (default: 1)")]
         page: usize,
     },
 }
 
 #[derive(Subcommand, Debug)]
 pub enum Nft {
-    #[command(about = "Mint a NFT, previously posted with '/meme post'")]
+    #[command(about = "Mint a MEME NFT, previously posted with '/meme post'")]
     Mint {
         #[arg(help = "Optional alphanumeric id of the meme returned by '/meme gen' (default: last meme posted)")]
         id: Option<String>,
     },
-    #[command(about = "Print the balance of a principal")]
+    #[command(about = "Print the MEME NFT balance of a principal")]
     Balance {
-        #[arg(short, long, help = "Optional principal of the owner (default = your OC user id")]
+        #[arg(short, long, help = "Optional principal of the owner (default: your OC user id")]
         owner: Option<String>,
     },
-    #[command(about = "Display the tokens of a principal")]
+    #[command(about = "Display the MEME NFTs of a principal")]
     Tokens {
-        #[arg(short, long, help = "Optional principal of the owner (default = your OC user id)")]
+        #[arg(short, long, help = "Optional principal of the owner (default: your OC user id)")]
         owner: Option<String>,
-        #[arg(default_value_t = 1, help = "Optional page number (default = 1)")]
+        #[arg(default_value_t = 1, help = "Optional page number (default: 1)")]
         page: usize,
     },
-    #[command(about = "Transfer a token to another principal")]
+    #[command(about = "Transfer a MEME NFT to another principal")]
     Transfer {
         #[arg(help = "Token id, as returned by '/meme nft tokens'")]
         id: NftId,
         #[arg(help = "Principal of the recipient")]
         to: String,
     },
-    #[command(about = "Display logs of NFT transactions")]
+    #[command(about = "Display logs of MEME NFT transactions")]
     Logs {
-        #[arg(default_value_t = 1, help = "Optional page number (default = 1)")]
+        #[arg(default_value_t = 1, help = "Optional page number (default: 1)")]
         page: usize,
     },
-    #[command(about = "Print the status of the NFT collection")]
+    #[command(about = "Print the status of the MEME NFT collection")]
     Status,
 }
