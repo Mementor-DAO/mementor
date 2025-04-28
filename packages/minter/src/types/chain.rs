d6bcc19c9bcf0f0bf1f491552eeb4c8f5462bddd
av1ctor <av1ctortv@gmail.com>
2025-04-28 15:17:56 -0300

    chore: block time is now the same as bitcoin (15 min)

--- packages/minter/src/types/chain.rs
+++ packages/minter/src/types/chain.rs
@@ -3,8 +3,8 @@ use crypto_bigint::U256;
 use serde::{Deserialize, Serialize};
 use super::block_hash::{BlockHash, BlockId};
 
-pub const BLOCK_TIME: u64 = 60 * 3; // block time in secs (5x faster than bitcoin)
-pub const INIT_BLOCK_REWARD: u64 = 10_00000000; // initial reward (5x less than bitcoin)
+pub const BLOCK_TIME: u64 = 60 * 15; // block time in secs (same as bitcoin)
+pub const INIT_BLOCK_REWARD: u64 = 50_00000000; // initial reward (same as bitcoin)
 pub const BLOCKS_PER_HALVING: u64 = (60 * 60 * 24 * 288) / BLOCK_TIME; // halving every 288 days (5x faster than bitcoin)
 
 #[derive(Clone, Serialize, Deserialize, CandidType)]
